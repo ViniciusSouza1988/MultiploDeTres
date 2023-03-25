62ebc62552f2d5f200d095c2a8944ed021263d60
Sapo Boi <Sapo Boi@DESKTOP-41MDM8D>
2023-03-25 12:08:33 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int quantidadeMultiplosDeTres = 0;
+int numeroMaximoParaTestar = 89781;
+for (int i = 0; i <= numeroMaximoParaTestar; i += 3)
+{
+    if (i % 3 == 0)
+        quantidadeMultiplosDeTres++;
+}
+
+Console.WriteLine("Existem " + quantidadeMultiplosDeTres + " Multiplos de 3 de 0 a " + numeroMaximoParaTestar);
